a8f9f7122f51a8c0947367a13039a4500299644c
justanarthur <kozubov.arthur@gmail.com>
2025-04-08 17:11:49 +0200

    feat: add metadata files and enhance API documentation chore: added documentation with link to swagger one

--- docs/app/layout.config.tsx
+++ docs/app/layout.config.tsx
@@ -1,4 +1,6 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared"
+import Image from "next/image"
+import logo from "./icon.png"
 
 /**
  * Shared layout configurations
@@ -11,15 +13,7 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <title>Logo</title>
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
+        <Image src={logo} alt="Slash Logo" width={20} height={20} />
         Slash
       </>
     )
